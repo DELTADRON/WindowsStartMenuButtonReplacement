04ad119075771a256734308ad1b32a7d475ded68
DELTADRON <79601050+DELTADRON@users.noreply.github.com>
2025-01-05 12:01:24 +0100

    Source code of files Program and TaskbarModifier

---
+++ src/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
+
+
+namespace WinStartMenuReplacement
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // Best resolution of image that you can give is 16x16px with colors black, white and transperent
+            string Winlogon = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon";
+            if (args.Length == 0)
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog
+                {
+                    Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp",
+                    Title = "Choose an image for the Start menu button"
+                };
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK) Start_Changing(openFileDialog.FileName);
+                else Console.WriteLine("No file selected.");
+
+            }
+            else if (args[0].ToLower() == "makeauto")
+            {
+                string image = args[1], app = Application.ExecutablePath;
+                var hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+
+                using (RegistryKey key = hklm.OpenSubKey(Winlogon, true))
+                {
+                    key.SetValue("Shell", $"{key.GetValue("Shell")}, \"{app}\" \"{image}\"", RegistryValueKind.String);
+                    key.Close();
+                }
+            }
+            else if (args[0].ToLower() == "rmauto")
+            {
+                string image = args[1], app = Application.ExecutablePath;
+                var hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+
+                using (RegistryKey key = hklm.OpenSubKey(Winlogon, true))
+                {
+                    key.SetValue("Shell", $"{key.GetValue("Shell").ToString().Replace($", \"{app}\" \"{image}\"", "")}", RegistryValueKind.String);
+                    key.Close();
+                }
+            }
+            else if (File.Exists(args[0])) Start_Changing(args[0]); 
+            else MessageBox.Show("File not found!");
+            
+        }
+        static void Start_Changing(string path)
+        {
+            StartButtonModifier modifier = new StartButtonModifier();
+
+            switch (Path.GetExtension(path).ToLower())
+            {
+                case ".png": break;
+                case ".jpg": break;
+                case ".jpeg": break;
+                case ".bmp": break;
+                default: Application.Exit(); break;
+            }
+
+            for (int i = 1; i <= 7; i++)
+            {
+                byte[] bitmapBytes = modifier.ConvertToBitmapBytes(new Bitmap(path), 16, 16); // Corect modifier.ConvertPngToBitmapBytes(pngPath, 16, 16);
+                modifier.ModifyStyle("TaskbarPearl", i, 0, 0, bitmapBytes);
+            }
+        }
+
+    }
+}
---
+++ src/TaskbarModifier.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace WinStartMenuReplacement
+{
+    class StartButtonModifier
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("uxtheme.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern IntPtr OpenThemeDataForDpi(IntPtr hwnd, string pszClassList, uint dpi);
+
+        [DllImport("uxtheme.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern int GetThemeBitmap(IntPtr hTheme, int iPartId, int iStateId, int iPropId, uint dwFlags, out IntPtr phBitmap);
+
+        [DllImport("uxtheme.dll", SetLastError = true)]
+        private static extern void CloseThemeData(IntPtr hTheme);
+
+        [DllImport("user32.dll")]
+        private static extern uint GetDpiForWindow(IntPtr hWnd);
+
+        [DllImport("gdi32.dll", SetLastError = true)]
+        private static extern int GetObject(IntPtr hObject, int nSize, ref BITMAP lpObject);
+
+        [DllImport("gdi32.dll", SetLastError = true)]
+        private static extern int GetBitmapBits(IntPtr hbmp, int cbBuffer, byte[] lpvBits);
+
+        [DllImport("gdi32.dll", SetLastError = true)]
+        private static extern int SetBitmapBits(IntPtr hbmp, int cbBuffer, byte[] lpvBits);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct BITMAP
+        {
+            public int bmType;
+            public int bmWidth;
+            public int bmHeight;
+            public int bmWidthBytes;
+            public ushort bmPlanes;
+            public ushort bmBitsPixel;
+            public IntPtr bmBits;
+        }
+
+        private const int GBF_DIRECT = 0x00000001;
+
+        public byte[] ConvertToBitmapBytes(Bitmap img, int width, int height)
+        {
+            using (Bitmap bitmap = new Bitmap(img, new Size(width, height)))
+            {
+
+                BitmapData bmpData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, bitmap.PixelFormat);
+                int byteCount = bmpData.Stride * bitmap.Height;
+                byte[] bitmapBytes = new byte[byteCount];
+                Marshal.Copy(bmpData.Scan0, bitmapBytes, 0, byteCount);
+                bitmap.UnlockBits(bmpData);
+
+                return bitmapBytes;
+            }
+        }
+
+        private bool UpdateBitmap(IntPtr hBitmap, byte[] bitmapBytes)
+        {
+            BITMAP bm = new BITMAP();
+            GetObject(hBitmap, Marshal.SizeOf(typeof(BITMAP)), ref bm);
+
+            if (hBitmap == IntPtr.Zero || bm.bmBitsPixel != 32 || bm.bmWidth > 64)
+            {
+                return false;
+            }
+
+            SetBitmapBits(hBitmap, bitmapBytes.Length, bitmapBytes);
+            return true;
+        }
+
+        public void ModifyStyle(string classList, int partId, int stateId, int propId, byte[] bitmapBytes)
+        {
+            IntPtr hTheme = OpenThemeDataForDpi(IntPtr.Zero, classList, GetDpiForWindow(IntPtr.Zero));
+            if (hTheme == IntPtr.Zero)
+            {
+                MessageBox.Show("Failed to open theme data.");
+                return;
+            }
+
+            if (GetThemeBitmap(hTheme, partId, stateId, propId, GBF_DIRECT, out IntPtr hBitmap) != 0 || hBitmap == IntPtr.Zero)
+            {
+                MessageBox.Show("Failed to get start menu bitmap.");
+                CloseThemeData(hTheme);
+                return;
+            }
+
+            if (UpdateBitmap(hBitmap, bitmapBytes))
+            {
+                Console.WriteLine("The start menu bitmap has been modified.");
+            }
+            else
+            {
+                MessageBox.Show("Failed to modify start menu bitmap.");
+            }
+
+            CloseThemeData(hTheme);
+        }
+    }
+}
